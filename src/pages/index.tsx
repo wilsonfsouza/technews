1d0c01a39b8ae683e78b24166c92d1e2a17ed851
Wilson Franca <wilson.franca.92@gmail.com>
2021-04-04 19:31:37 -0700

    feat: base app.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | tech.news</title>
+      </Head>
+      <h1>
+        Home
+      </h1>
+    </>
+  )
+}
