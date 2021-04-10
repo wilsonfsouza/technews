5ad70677fbe8ade6ed305a8374f53de6caf33acb
Wilson Franca <wilson.franca.92@gmail.com>
2021-04-09 20:59:30 -0700

    feat: added fauna db service and added a new user to the database.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query } from 'faunadb';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -9,4 +11,40 @@ export default NextAuth({
             scope: 'read:user'
         }),
     ],
+    // jwt: {
+    //     signingKey: process.env.SIGNIN_KEY
+    // },
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        query.Create(
+                            query.Collection('users'),
+                            { data: { email } }
+                        ),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        }
+    }
 })
