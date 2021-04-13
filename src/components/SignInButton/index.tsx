b2326ee5ec8f427ad85037563cba97d45adc9cdb
Wilson Franca <wilson.franca.92@gmail.com>
2021-04-12 22:05:21 -0700

    chore: removed console.log.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 
 export function SignInButton() {
     const [session] = useSession();
-    console.log(session);
+
     return session ? (
         <button
             type="button"
